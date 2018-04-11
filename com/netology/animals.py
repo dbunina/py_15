633f9f403a9e03bc3e4c69c73e8694d04297aafb
Diana Bunina <d.bunina@hh.ru>
2018-04-11 13:56:05 +0300

    fix according to pep8 guidelines; add .gitignore

--- com/netology/animals.py
+++ com/netology/animals.py
@@ -5,8 +5,14 @@ class Animal:
     weight = 0
     position = 0
 
+    def __init__(self):
+        self.color = None
+        self.age = 0
+        self.weight = 0
+        self.position = 0
+
     def reproduce(self):
-        pass # abstract
+        pass  # abstract
 
     def talk(self, sound):
         print(sound)
@@ -16,28 +22,38 @@ class Animal:
         print('Moving to position {}'.format(position))
 
     def eat(self, amount_of_food):
-        self.weight += amount_of_food / 2 # let's imagine that 50% of food is absorbed
+        self.weight += amount_of_food / 2  # let's imagine that 50% of food is absorbed
+
 
 class Mammal(Animal):
 
     def reproduce(self):
         print('Giving birth to a new mammal...')
 
+
 class Cow(Mammal):
+
     def talk(self, sound='Muu-u-u!'):
         super(Cow, self).talk(sound)
 
+
 class Goat(Mammal):
+
     def talk(self, sound='Bie-e-e!'):
         super(Goat, self).talk(sound)
 
+
 class Sheep(Mammal):
+
     def talk(self, sound='Mie-e-e!'):
         super(Sheep, self).talk(sound)
 
+
 class Pig(Mammal):
+
     def talk(self, sound='Hru-hru!'):
         super(Pig, self).talk(sound)
+
 
 class Bird(Animal):
 
@@ -59,6 +75,7 @@ class Bird(Animal):
             self.altitude -= altitude
             print('The bird is descending to altitude {}'.format(altitude))
 
+
 class Duck(Bird):
 
     def talk(self, sound='Quack-quack!'):
@@ -67,10 +84,12 @@ class Duck(Bird):
     def swim(self):
         print('The duck is swimming...')
 
+
 class Chicken(Bird):
 
     def talk(self, sound='Ko-ko-ko!'):
         super(Chicken, self).talk(sound)
+
 
 class Goose(Bird):
 
